b553e880556d29761cd297b51a4329c62fe6ba93
antoine.desaintmartin <a8desain@enib.fr>
2022-09-07 23:36:53 -0400

    Ajout de la classe vecteur3D commencee + creation classe particule (vierge) + creation fichier permettant les operations d'integration

---
+++ moteur_physique/moteur_physique/Particule.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//Implémenter la classe contenant les différents types de particules (balles, boulets, laser...) et les comportements différents de ces objets
+
+class Particule
+{
+};
+
---
+++ moteur_physique/moteur_physique/Vecteur3D.cpp
@@ -0,0 +1,3 @@
+#include "Vecteur3D.h"
+
+
