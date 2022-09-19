fc5e434c71948b187aa46610ae6786531299abef
antoine.desaintmartin <a8desain@enib.fr>
2022-09-19 08:16:56 -0400

    mise à niveau de ma branche

---
+++ moteur_physique/moteur_physique/Vecteur3D.h
@@ -0,0 +1,229 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+
+#ifndef VECTEUR3D_HPP
+#define VECTEUR3D_HPP
+
+namespace moteurJeux {
+
+//Our 3D vector just contain 3 coordinates. A lot of method can be usefull, get magnitude, normalize it, get his angle with the origin
+class Vecteur3D
+	{
+
+	//attributes
+	public:
+		
+		double x;
+		double y;
+		double z;
+
+	//constructors
+	public:
+
+		//default constructor that creates a (0,0,0) Vector3D
+		Vecteur3D() : x(0), y(0), z(0) 
+		{
+			//nothing more to be had here
+		}
+
+		//constructors with parameters to create a new one
+		Vecteur3D(const double xCoord, const double yCoord, const double zCoord) : x(xCoord), y(yCoord), z(zCoord) 
+		{
+			//nothing more to be had here
+		}
+
+	//methods
+	public :
+
+		//DISPLAY OUR VECTOR
+		void display() {
+			std::cout << "(" << x << "," << y << "," << z << ")" << std::endl;
+		}
+
+		//NORMALIZATION
+
+		//normalize a vector means to obtain his coordinates between 0 and 1, in the unit circle. We normalize only if the actual Vector3D is not in the unit circle.
+		/*
+		* if it's in the unit circle, that means the magnitude (the lenght) is < 1. So we need to verify the magnitude before normalizing it.
+		* if it's not, it means that the max value of the vector is equal to the magnitude (ex : magnitude of 4 -> sqrt(0*0 + 4*4 + 0*0) = 4). So we need to divide by magnitude 
+		* each parameter to fit them between 0 and 1.
+		*/
+		void get_normalization()
+		{
+			if (get_magnitude() > 1)
+			{
+				//x = x / get_magnitude();
+				//y = y / get_magnitude();
+				//z = z / get_magnitude();
+				(*this) *= ((double)1) / get_magnitude();
+			}
+		}
+
+		//get the normalized vector of a given Vecteur3D
+		Vecteur3D get_normalization(Vecteur3D& vect)
+		{
+			if (get_magnitude(vect) > 1)
+			{
+				return vect *= ((double)1) / get_magnitude(vect);
+			}
+		}
+
+		//MAGNITUDE
+
+		//get the norm of a vector means to calculate his lenght, get the magnitude of this vector
+		double get_magnitude()
+		{
+			return sqrt(x * x + y * y + z * z);
+		}
+
+
+		//get the norm of a vector Vecteur3D
+		double get_magnitude(Vecteur3D& vect)
+		{
+			return sqrt(vect.x * vect.x + vect.y * vect.y + vect.z * vect.z);
+ 		}
+
+		//ADDITION AND MULTIPLICATION BY SCALAR VALUE
+
+		//add a to our Vecteur3D another one that is multiplied by a scalar
+		void addMultipliedVector(Vecteur3D& vect, double facteur)
+		{
+			Vecteur3D multiplied = vect * facteur;
+			(*this) += multiplied;
+		}
+
+		//multiply our Vecteur3D with another one
+		Vecteur3D multiplyBy(Vecteur3D& vect)
+		{
+			return (*this) *= vect;
+		}
+
+		//VECTORIAL PRODUCT
+
+		//get the vectorial product between our vector and another one (axb means to obtain a vector orthogonal with a and b)
+		Vecteur3D vectorialProduct(Vecteur3D& vect)
+		{
+			return Vecteur3D(y*vect.z - z*vect.y, z*vect.x - x*vect.z, x*vect.y - y*vect.x);
+		}
+
+		//get vectorial product between 2 specified vectors
+		Vecteur3D vectorialProduct(Vecteur3D& vect1, Vecteur3D& vect2)
+		{
+			return Vecteur3D(vect1.y * vect2.z - vect1.z * vect2.y, vect1.z * vect2.x - vect1.x * vect2.z, vect1.x * vect2.y - vect1.y * vect2.x);
+		}
+		
+		//SCALAR PRODUCT
+
+		//get the scalar product between our vector and another one (a.b means the size of b projected on a)
+		double scalarProduct(Vecteur3D& vect)
+		{
+			return x * vect.x + y * vect.y + z * vect.z;
+		}
+		
+		//CREATE AN ORTHONOMAL BASIS
+
+		struct orthonormalBasis //9 points int the basis, 3 for vect(x), 3 for vect(y), 3 for vect(z)
+		{
+			double basis[9];
+		};
+
+		//return an orthonormal basis, 3 vectors.
+		orthonormalBasis createOrthonormalBasis(Vecteur3D& vect1, Vecteur3D& vect2)
+		{
+			vect1 = get_normalization(vect1);
+			vect2 = get_normalization(vect2);
+			Vecteur3D vect3 = vectorialProduct(vect1,vect2);
+			if (!isNULL(vect3))
+			{
+				vect2 = vectorialProduct(vect1, vect3);
+				orthonormalBasis myBase;
+				double list[9] = { vect1.x, vect1.y, vect1.z, vect2.x, vect2.y, vect2.z, vect3.x, vect3.y, vect3.z };
+				memcpy(myBase.basis, list, sizeof(list));
+				return myBase;
+			}
+
+		}
+
+
+		bool isNULL(Vecteur3D& vect1)
+		{
+			if (vect1.x == 0)
+				if (vect1.y == 0)
+					if (vect1.z == 0)
+						return true;
+			return false;
+		}
+
+		//OPERATORS ON VECTEUR3D
+
+		Vecteur3D operator*(double value)
+		{
+			x * value;
+			y * value;
+			z * value;
+		}
+
+		Vecteur3D operator*=(Vecteur3D& vect)
+		{
+			x * vect.x;
+			y * vect.y;
+			z * vect.z;
+		}
+
+		//be able to multiplied our Vecteur3D with a scalar value (double value here) //void operator*=(double value)
+		Vecteur3D operator*=(double value)
+		{
+			x * value;
+			y * value;
+			z * value;
+		}
+
+		//return a copy of a Vecteur3D of a multiplication between a scalar value (double value here) and our Vecteur3D
+		Vecteur3D operator*=(double value) const //avoid our object to be modified
+		{
+			return Vecteur3D(x * value, y * value, z * value);
+		}
+
+		void operator+(Vecteur3D& v) //get "v" attribute by the memory reference
+		{
+			x += v.x;
+			y += v.y;
+			z += v.z;
+		}
+
+		//be able to add a Vecteur3D to our Vecteur3D
+		void operator+=(Vecteur3D& v) //get "v" attribute by the memory reference
+		{
+			x += v.x;
+			y += v.y;
+			z += v.z;
+		}
+
+		//return a copy of a Vecteur3D of an addition between a Vecteur3D added to our Vecteur3D
+		Vecteur3D operator+=(Vecteur3D& v) const 
+		{
+			return Vecteur3D(x + v.x, y + v.y, z + v.z);
+		}
+
+		//be able to substract a Vecteur3D to our Vecteur3D
+		void operator-=(Vecteur3D& v)
+		{
+			x -= v.x;
+			y -= v.y;
+			z -= v.z;
+		}
+
+		//return a copy of a Vecteur3D of an substraction between a Vecteur3D substracted to our Vecteur3D
+		Vecteur3D operator-=(Vecteur3D& v) const
+		{
+			return Vecteur3D(x - v.x, y - v.y, z - v.z);
+		}
+
+	};
+
+} //namespace moteurJeux
+
+#endif VECTEUR3D_HPP
+
+
