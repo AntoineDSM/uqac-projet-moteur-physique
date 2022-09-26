9b07d6a5651806e19b62b6e8a2d6ad039ad37797
fa0z <58564474+fa0z@users.noreply.github.com>
2022-09-26 10:06:18 -0400

    Create NaiveParticleConctactGenerator.h

---
+++ moteur_physique/moteur_physique/NaiveParticleConctactGenerator.h
@@ -0,0 +1,8 @@
+class NaiveParticleContactGenerator : public ParticleContactGenerator
+{
+// Particle radius
+float radius;
+// particles that could collide together 
+std: :vector‹Particle*> particles;
+// iterate through every pair of particles to check their penetration
+unsigned int addContact (ParticleContact* contact, unsigned int limit) const;};
