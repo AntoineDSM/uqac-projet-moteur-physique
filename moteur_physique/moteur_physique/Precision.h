45abc066717f45d18556c4658da6508cd773ef01
Vicou <victor.guiraud16@gmail.com>
2022-10-12 15:30:56 -0400

    jn

--- moteur_physique/moteur_physique/Precision.h
+++ moteur_physique/moteur_physique/Precision.h
@@ -14,6 +14,44 @@
 
 namespace Precision {
 
+    inline double carre(double x)
+    {
+        return x * x;
+    }
+
+    inline void swapNums(int nums[],
+        int first, int second, int secondArray[])
+    {
+        int curr = nums[first];
+        int currSecond = secondArray[first];
+        nums[first] = nums[second];
+        secondArray[first] = secondArray[second];
+        nums[second] = curr;
+        secondArray[second] = currSecond;
+    }
+
+    inline void sort(int num[], int len, int secondArray[])
+    {
+        bool isSwapped;
+        for (int i = 0; i < len; i++)
+        {
+            isSwapped = false;
+            for (int j = 1; j < len - i; j++)
+            {
+                if (num[j] < num[j - 1])
+                {
+                    swapNums(num, j, (j - 1), secondArray);
+                    isSwapped = true;
+                }
+            }
+            if (!isSwapped)
+            {
+                break;
+            }
+        }
+    }
+
+
 
 }//namespace Precision
 
