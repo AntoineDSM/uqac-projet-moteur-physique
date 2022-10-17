8d4166e864ba175b3f801c3c70e9e5f621a6328c
Axel GUZA <axel.guza@etu.unilim.fr>
2022-10-17 15:34:08 -0400

    implémentation partiel du contact au repos

--- moteur_physique/moteur_physique/WallContact.cpp
+++ moteur_physique/moteur_physique/WallContact.cpp
@@ -6,14 +6,28 @@ bool WallContactGenerator::EstAuRepos(Particle p, TimingData t) {
 	Vector3D p1 = p.getAcceleration() * t.lastFrameDuration;
 	Vector3D p2 = p.getVelocity();
 
-	WallContactGenerator w;
-
 	if (p1.x > p2.x && p1.y > p2.y && p1.z > p2.z)
 	{
-		
+		return true;
 	}
+	return false;
+};
+
+void WallContactGenerator::repos(Particle p, ParticleGravity g) {
+	Vector3D vecZero;
+	vecZero.x, vecZero.y, vecZero.z = 0;
+
+	//Nous voulon trouver la vélocité relative dans la direction de la normal
+	Vector3D velocityRelative = p.getAcceleration() - p.getVelocity();
 
 
-
-	return true;
+	//On recherche ici de savoir si cela est dû à la gravité
+	if (velocityRelative == g) {
+		//ignoré la vélocité dans les autres direction
+		//TO DO
+	}
+	else {
+		p.setVelocity(vecZero);
+	}
+	
 };
